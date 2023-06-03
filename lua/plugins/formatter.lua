0eb1df7de432ad6d26f58be4d5f513947cef98f9
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2023-06-03 16:25:49 +0200

    feat(plugins): added formatter.nvim

---
+++ lua/plugins/formatter.lua
@@ -0,0 +1,31 @@
+return {
+    "mhartington/formatter.nvim",
+    event = "BufWrite",
+    config = function()
+        vim.g.format_on_save = true
+
+        local formatter = require("formatter")
+        formatter.setup({
+            filetype = {
+                lua = { require("formatter.filetypes.lua").stylua },
+                c = { require("formatter.filetypes.c").clangformat },
+                cpp = { require("formatter.filetypes.cpp").clangformat },
+                nix = { require("formatter.filetypes.nix").nixpkgs_fmt },
+                yaml = { require("formatter.filetypes.yaml").prettier },
+                json = { require("formatter.filetypes.json").prettier },
+                python = { require("formatter.filetypes.python").black },
+                ["*"] = { require("formatter.filetypes.any").remove_trailing_whitespace },
+            },
+        })
+
+        vim.api.nvim_create_autocmd("BufWritePost", {
+            group = vim.api.nvim_create_augroup("my_format_write", { clear = true}),
+            pattern = "*",
+            callback = function()
+                if vim.g.format_on_save then
+                    vim.cmd("silent! FormatWrite")
+                end
+            end,
+        })
+    end,
+}
