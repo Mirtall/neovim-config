d1227f0b9705ff70d84081d7d09a7f24e851aac7
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2023-06-09 17:20:17 +0200

    feat(lsp): configured definition & stuffs

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -16,5 +16,17 @@ return {
                 capabilities = vim.deepcopy(capabilities),
             }, config))
         end
-    end
-}
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+                local opts = { buffer = ev.buf }
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                vim.keymap.set('n', "<leader>a", vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+            end
+        })
+        end
+    }
