35eaf0b6b490121f5b95f2523fc419559b57b80c
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2023-08-07 18:46:52 +0200

    feat(cmp): added path as a source

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -22,9 +22,9 @@ return {
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
-                { name = 'nvim_lsp_signature_help' },
-            }, {
-                { name = "buffer"}
+                { name = "nvim_lsp_signature_help" },
+                { name = "path" },
+                { name = "buffer"},
             }),
 
             mapping = cmp.mapping.preset.insert({
