15e993fc98567301f8bb55c91a1742f07a9e5d39
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2023-09-27 15:22:39 +0200

    feat(git): added fugitive (missed the blame & mergetool)

---
+++ lua/plugins/git.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+        },
+        cmd = "Neogit",
+        keys = {
+            { "<leader>g", "<CMD>Neogit<CR>", desc = "neogit" },
+        },
+        opts = {
+            integrations = {
+                diffview = true,
+            },
+            disable_commit_confirmation = true,
+        },
+    },
+    {
+        "tpope/vim-fugitive",
+        cmd = "Git",
+    },
+}
