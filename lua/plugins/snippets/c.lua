201dff26471efabdd92decd9ea0138a0f92da87c
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2023-11-24 15:54:57 +0100

    feat(snippet): added luasnip & snippet for C/C++

---
+++ lua/plugins/snippets/c.lua
@@ -0,0 +1,53 @@
+local ls = require("luasnip")
+
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+return {
+    ls.snippet("inc", fmt('#include "{}"', { i(1) })),
+    ls.snippet("Inc", fmt('#include <{}>', { i(1) })),
+    ls.snippet(
+        "main",
+        fmt(
+        [[
+            int main(int argc, char *argv[])
+            {{
+                {}
+                return 0;
+            }}
+        ]],
+        { i(1) }
+        )
+    ),
+
+    ls.snippet(
+        "mainv",
+        fmt(
+        [[
+            int main(void)
+            {{
+                {}
+                return 0;
+            }}
+        ]],
+        { i(1) }
+        )
+    ),
+    ls.snippet(
+        "fore",
+        fmt(
+            [[
+                for ({}; {}; {})
+                {{
+                    {}
+                }}
+            ]],
+            {
+                i(1),
+                i(2),
+                i(3),
+                i(4),
+            }
+        )
+    ),
+}
---
+++ lua/plugins/snippets/init.lua
@@ -0,0 +1,49 @@
+return {
+    "L3MON4D3/LuaSnip",
+    keys = {
+        {
+            "<C-j>",
+            function()
+                if require("luasnip").jumpable(1) then
+                    require("luasnip").jump(1)
+                end
+            end,
+            mode = { "i", "s" },
+        },
+        {
+            "<C-k>",
+            function()
+                if require("luasnip").jumpable(-1) then
+                    require("luasnip").jump(-1)
+                end
+            end,
+            mode = { "i", "s" },
+        },
+        {
+            "<C-l>",
+            function()
+                if require("luasnip").choice_active() then
+                    require("luasnip").change_choice(1)
+                end
+            end,
+            mode = { "i", "s" },
+        },
+        {
+            "<C-h>",
+            function()
+                if require("luasnip").choice_active() then
+                    require("luasnip").change_choice(-1)
+                end
+            end,
+            mode = { "i", "s" },
+        },
+    },
+    config = function(_, opts)
+        local ls = require("luasnip")
+        ls.setup(opts)
+        for _, lang in pairs({ "c" }) do
+            ls.add_snippets(lang, require("plugins.snippets." .. lang))
+        end
+        ls.add_snippets("cpp", require("plugins.snippets.c"))
+    end,
+}
