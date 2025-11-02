1154cee85825b364374c396cb7b501a3bc90a944
Ethan Zouzoulkowsky <ethan.zouzoulkowsky@gmail.com>
2025-11-02 14:22:04 +0100

    feat: added copilot

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -12,6 +12,18 @@ return {
     config = function()
         local cmp = require("cmp")
 
+        local has_words_before = function()
+            if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+                return false
+            end
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0
+                and vim.api
+                        .nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]
+                        :match("^%s*$")
+                    == nil
+        end
+
         cmp.setup({
             snippet = {
                 expand = function(args)
@@ -25,6 +37,7 @@ return {
                 { name = "nvim_lsp_signature_help" },
                 { name = "path" },
                 { name = "buffer" },
+                { name = "copilot" },
             }),
 
             mapping = cmp.mapping.preset.insert({
@@ -32,32 +45,37 @@ return {
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(),
                 ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
+                ["<Tab>"] = vim.schedule_wrap(function(fallback)
+                    if cmp.visible() and has_words_before() then
+                        cmp.select_next_item({
+                            behavior = cmp.SelectBehavior.Select,
+                        })
                     else
                         fallback()
                     end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
+                end),
+                ["<S-Tab>"] = vim.schedule_wrap(function(fallback)
+                    if cmp.visible() and has_words_before() then
+                        cmp.select_prev_item({
+                            behavior = cmp.SelectBehavior.Select,
+                        })
                     else
                         fallback()
                     end
-                end, { "i", "s" }),
+                end),
             }),
 
             formatting = {
                 format = require("lspkind").cmp_format({
+                    mode = "symbol",
                     with_text = true,
                     menu = {
                         nvim_lsp = "[LSP]",
-                        path = "[path]",
                         luasnip = "[snip]",
-                        calc = "[calc]",
                         nvim_lsp_signature_help = "[sign]",
+                        path = "[path]",
                         buffer = "[buf]",
+                        copilot = "",
                     },
                 }),
             },
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        opts = {
+            suggestion = {
+                enabled = false,
+            },
+            panel = { enabled = false },
+        },
+    },
+    {
+        "zbirenbaum/copilot-cmp",
+        event = "InsertEnter",
+        config = function()
+            require("copilot_cmp").setup()
+        end,
+    },
+}
